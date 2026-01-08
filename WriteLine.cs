f6920454be5719ed838b07d6d2903e594a4ef64e
unfuny1337 <120639123+unfuny1337@users.noreply.github.com>
2026-01-09 01:49:46 +0200

    Reupload

---
+++ WriteLine.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PasswordGenerator
+{
+    internal class WriteClass
+    {
+        public static void WriteLine(string text, ConsoleColor color)
+        {
+            Console.ForegroundColor = color;
+            Console.WriteLine(text);
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+
+        public static void Write(string text, ConsoleColor color)
+        {
+            Console.ForegroundColor = color;
+            Console.Write(text);
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+    }
+}
